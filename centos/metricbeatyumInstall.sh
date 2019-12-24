0a2d7515d55cc5e7d1c47cbd67ed501a66493105
SulaymanSaleem <39617382+SulaymanSaleem@users.noreply.github.com>
2019-12-24 16:40:19 +0000

    Update metricbeatyumInstall.sh
    adding enable to metricbeatyumInstall.sh

--- centos/metricbeatyumInstall.sh
+++ centos/metricbeatyumInstall.sh
@@ -3,6 +3,7 @@
 sudo rpm --import https://packages.elastic.co/GPG-KEY-elasticsearch
 sudo cp elastic.repo /etc/yum.repos.d/
 sudo yum install metricbeat -y
+sudo systemctl enable metricbeat
 
 sudo cp metricbeat.yml /etc/metricbeat/metricbeat.yml
 
